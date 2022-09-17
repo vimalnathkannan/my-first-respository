8048838b76c7aa4fd1483f90a3391cf14226c043
vimal <111766754+vimalnathkannan@users.noreply.github.com>
2022-09-17 13:07:42 +0530

    added int value

--- src/vimal.java
+++ src/vimal.java
@@ -2,7 +2,11 @@
 public class vimal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a,b;
+		int a=10;
+		int b=10;
+	        int c=a+b;
+		System.out.println(c);
 
 	}
 
